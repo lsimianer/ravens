619abbbd3285fab37535479639016ce90948c37e
Lukas Simianer <lsimianer@gmail.com>
2020-07-30 15:24:10 -0500

    lets see mobile

--- app.js
+++ app.js
@@ -25,3 +25,12 @@ const toggleBorder = () => {
     }
 }
 
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+  
