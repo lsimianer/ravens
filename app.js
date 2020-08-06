67fad664be662a50cdb7f2de0f45e64c07a91947
Lukas Simianer <lsimianer@gmail.com>
2020-08-06 09:47:52 -0500

    update calc

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
-
-
-
 const toggleText = () => {
     let myDom = document.querySelectorAll('.instructional')
 
-    for(var i = 0; i < myDom.length; i++){
+    for (var i = 0; i < myDom.length; i++) {
         myDom[i].classList.toggle('biggerText')
     }
 }
@@ -12,7 +9,7 @@ const toggleText = () => {
 const toggleContrast = () => {
     let myDom = document.querySelectorAll('.instructional')
 
-    for(var i = 0; i < myDom.length; i++){
+    for (var i = 0; i < myDom.length; i++) {
         myDom[i].classList.toggle('Contrast')
     }
 }
@@ -20,30 +17,60 @@ const toggleContrast = () => {
 const toggleBorder = () => {
     let myDom = document.querySelectorAll('.instructional')
 
-    for(var i = 0; i < myDom.length; i++){
+    for (var i = 0; i < myDom.length; i++) {
         myDom[i].classList.toggle('border')
     }
 }
 
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
-  }
-  
-  /* Set the width of the side navigation to 0 */
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
-  
-  const spinnyThingy = () => {
-      console.log('tester')
-     let menu = document.getElementById('burgerBtn');
-     menu.classList.toggle('down')
-  }
+}
 
-  const calculate = () => {
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+const spinnyThingy = () => {
+    console.log('tester')
+    let menu = document.getElementById('burgerBtn');
+    menu.classList.toggle('down')
+}
+
+const calculate = () => {
     event.preventDefault()
     let updateFrequency = document.getElementById('updateFreq').value
     let pageCount = document.getElementById('pageCount').value
     let websiteUse = document.getElementById('websiteUse').value
-      console.log('shit',websiteUse)
-  }
+    //cal audit total
+    let auditCalc = (parseInt(websiteUse, 10) * parseInt(pageCount, 10))
+    // calc remediation
+    let remediationCalc
+    if (websiteUse === '550') {
+        remediationCalc = 1020
+    }
+    if (websiteUse === '570') {
+        remediationCalc = 2050
+    }
+    if (websiteUse === '600') {
+        remediationCalc = 4000
+    }
+    // calc maintenance 
+    let maintenanceCalc
+    if (websiteUse === '550') {
+        maintenanceCalc = ((parseInt(pageCount, 10) * 57) * parseInt(updateFrequency, 10))
+    }
+    if (websiteUse === '570') {
+        maintenanceCalc = ((parseInt(pageCount, 10) * 110) * parseInt(updateFrequency, 10))
+    }
+    if (websiteUse === '600') {
+        maintenanceCalc = ((parseInt(pageCount, 10) * 150) * parseInt(updateFrequency, 10))
+    }
+    // total calc
+    let total = (auditCalc + maintenanceCalc + remediationCalc)
+
+    let auditTotal = document.getElementById('auditTotal').innerText = `$ ${auditCalc.toLocaleString()}`
+    let remediateTotal = document.getElementById('remediateTotal').innerText = `$ ${remediationCalc.toLocaleString()}`
+    let maintenanceTotal = document.getElementById('maintenanceTotal').innerText = `$ ${maintenanceCalc.toLocaleString()}`
+    let yearlyTotal = document.getElementById('yearlyTotal').innerText = `$ ${total.toLocaleString()}`
+}
